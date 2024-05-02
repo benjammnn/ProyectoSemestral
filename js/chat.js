72e4c04a3367851c9d86ed9b4cb90e3063b0f825
MaximilianoReszczynski <ma.reszczynski@duocuc.cl>
2024-05-02 13:25:08 -0400

    Ajuste de tamaño en el input y agregé el botón para enviar archivos, fotos,etc.

--- js/chat.js
+++ js/chat.js
@@ -14,4 +14,12 @@ function enviarMensaje() {
     var mensajeDiv = $('<div></div>').text(inputEnviar).addClass('mensaje-enviado');
     $('.overflow-auto').append(mensajeDiv);
     $("#chatInput").val("");
+    $("#chatInput").css('height', 'auto'); // Reset the height
 }
+var textarea = document.getElementById('chatInput');
+    textarea.addEventListener('input', autoResize, false);
+    
+    function autoResize() {
+        this.style.height = 'auto';
+        this.style.height = this.scrollHeight + 'px';
+    }
