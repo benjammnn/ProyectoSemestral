c1d77adb9ea12491973d3d25200f57ce48165b47
benjammnn <benja.arevalo.onetto@gmail.com>
2024-05-14 17:39:49 -0400

    detallitos

--- js/matcher.js
+++ js/matcher.js
@@ -1,11 +1,9 @@
-// 1. Implementando funcionalidades de responsividad
-
 const tl = new TimelineLite({ paused: true });
-// Animación de transición de entrada
+
 tl.fromTo(".carousel-item.active", { opacity: 0 }, { opacity: 1 }, 0.5, { ease: Power4.easeInOut });
-// Animación de transición de salida
+
 tl.fromTo(".carousel-item", { opacity: 1 }, { opacity: 0 }, 0.5, { ease: Power4.easeInOut });
-// Al cambiar de diapositiva, reproducir la animación
+
 $(".carousel").on("slid.bs.carousel", function (event) {
   tl.restart();
 });
@@ -31,16 +29,16 @@ buttons.forEach(button => {
     });
   });
 });
-// Adaptación del tamaño de las imágenes:
+
 window.addEventListener('resize', () => {
     const images = document.querySelectorAll('.card-img-top');
   
     images.forEach(image => {
-      const cardWidth = image.closest('.card').offsetWidth; // Obtener el ancho de la tarjeta
-      const aspectRatio = image.naturalWidth / image.naturalHeight; // Obtener la relación de aspecto de la imagen
+      const cardWidth = image.closest('.card').offsetWidth;
+      const aspectRatio = image.naturalWidth / image.naturalHeight;
   
-      const newWidth = Math.min(cardWidth, cardWidth * 0.8); // Limitar el ancho máximo de la imagen
-      const newHeight = newWidth / aspectRatio; // Calcular la nueva altura en función del ancho y la relación de aspecto
+      const newWidth = Math.min(cardWidth, cardWidth * 0.8);
+      const newHeight = newWidth / aspectRatio;
   
       gsap.to(image, {
         width: newWidth,
@@ -51,15 +49,13 @@ window.addEventListener('resize', () => {
     });
   });
   
-// Reorganización del contenido
 
   const carousel = document.querySelector('.carousel');
   window.addEventListener('resize', () => {
-    const breakpoint = 768; // Punto de ruptura para el cambio de diseño
+    const breakpoint = 768;
     const columns = carousel.querySelectorAll('.col');
 
     if (window.innerWidth < breakpoint) {
-      // Diseño para dispositivos móviles (una columna)
       columns.forEach(column => {
         carousel.appendChild(column);
       });
@@ -69,7 +65,6 @@ window.addEventListener('resize', () => {
         ease: "Power2.easeInOut"
       });
     } else {
-      // Diseño para escritorio (dos columnas)
       while (columns.length > 0) {
         const column1 = columns.shift();
         const column2 = columns.shift();
@@ -88,7 +83,7 @@ window.addEventListener('resize', () => {
     const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
   
     headings.forEach(heading => {
-      const fontSize = Math.max(16, Math.min(40, window.innerWidth / 100)); // Calcular el tamaño de fuente en función del ancho de la pantalla
+      const fontSize = Math.max(16, Math.min(40, window.innerWidth / 100));
       gsap.to(heading, {
         fontSize: fontSize + 'px',
         duration: 0.5,
