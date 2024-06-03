c9a91fab88cc5d65ba939b8501f839e6860c49f4
benjammnn <benja.arevalo.onetto@gmail.com>
2024-06-02 22:22:10 -0400

    avance(models.py terminado y migrado)

--- matcherProyect/settings.py
+++ matcherProyect/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myapp.middleware.ActualizarUltimaActividadMiddleware'
 ]
 
 ROOT_URLCONF = 'matcherProyect.urls'
--- myapp/models.py
+++ myapp/models.py
@@ -1,32 +1,50 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
-""" class Usuario ():
-    username = models.AutoField(primary_key=True)
-    nombre = models.TextField(blank=True, null=True)
-    edad = models.PositiveIntegerField(blank = True, null = True)
-    genero = models.CharField(max_length=20, blank=True, null=True)
+class Usuario(models.Model):
+    id_username = models.CharField(primary_key=True, max_length=15)
+    nombre = models.CharField(blank=False, null=False, max_length=15)
+    apellido = models.CharField(blank=False, null=False, max_length=15)
+    edad = models.PositiveIntegerField(blank=False, null=False, max_length=2)
+    id_genero = models.ForeignKey(
+        "Genero", on_delete=models.CASCADE, db_column="id_Genero"
+    )
+    email = models.EmailField(unique=True, max_length=50, blank=False, null=False)
+    ubicacion = models.CharField(blank=False, name=False, max_length=50)
     biografia = models.TextField(blank=True, null=True)
-    ubicacion = models.CharField(max_length=100, blank=True, null=True)
     fecha_registro = models.DateTimeField(auto_now_add=True)
-    ultima_actividad = models.DateTimeField(auto_now=True)
-
-    email = models.EmailField(unique = True)
+    ultima_actividad = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.username
+        return str(self.id_username)
+    
 
+class Genero(models.Model):
+    id_genero = models.AutoField(db_column="idGenero", primary_key=True)
+    genero = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.genero)
+    
 class Foto(models.Model):
-    usuario = models. ForeignKey(Usuario, on_delete=models.CASCADE, related_name='fotos')
-    ruta = models.ImageField(upload_to='fotos/')
-    fecha_subida = models.DateField(auto_now_add=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='myapp\static\myapp\img/')
+    fecha_subida = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Foto de {self.usuario.id_username}"
+    
 class Mensaje(models.Model):
     usuario_emisor = models.ForeignKey(Usuario, related_name='mensajes_enviados', on_delete=models.CASCADE)
     usuario_receptor = models.ForeignKey(Usuario, related_name='mensajes_recibidos', on_delete=models.CASCADE)
-    contenido = models.TextField()
+    contenido = models.TextField(blank=False, null=False)
     fecha_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Mensaje de {self.usuario_emisor.id_username} a {self.usuario_receptor}"
+        '''return f"Mensaje de {self.usuario_emisor} a {self.usuario_receptor}"'''
+
 
 class Match(models.Model):
     usuario_1 = models.ForeignKey(Usuario, related_name='matches_1', on_delete=models.CASCADE)
@@ -40,7 +58,7 @@ class Preferencia(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='preferencia')
     edad_minima = models.PositiveIntegerField(blank=True, null=True)
     edad_maxima = models.PositiveIntegerField(blank=True, null=True)
-    genero_preferido = models.CharField(max_length=20, blank=True, null=True)
+    id_genero = models.ForeignKey(
+        "Genero", on_delete=models.CASCADE, db_column="id_Genero"
+    )
     distancia_maxima = models.PositiveIntegerField(blank=True, null=True)
-
- """
