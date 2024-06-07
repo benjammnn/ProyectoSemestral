ab6ce99ce2abda7c61baa1a4ec0be9d9190bc7f7
Maxi <ma.reszczynski@duocuc.cl>
2024-06-07 11:41:45 -0400

    arreglo con el models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Genero, User, Foto, UsuarioSettings, Match, Mensaje, chat
 # Register your models here.
+
+admin.site.register(Genero)
+admin.site.register(User)
+admin.site.register(Foto)
+admin.site.register(UsuarioSettings)
+admin.site.register(Match)
+admin.site.register(Mensaje)
+admin.site.register(chat)
+
--- myapp/models.py
+++ myapp/models.py
@@ -2,63 +2,65 @@ from django.db import models
 from django.utils import timezone
 
 
-class Usuario(models.Model):
-    id_username = models.CharField(primary_key=True, max_length=15)
-    nombre = models.CharField(blank=False, null=False, max_length=15)
-    apellido = models.CharField(blank=False, null=False, max_length=15)
-    edad = models.PositiveIntegerField(blank=False, null=False, max_length=2)
-    id_genero = models.ForeignKey(
-        "Genero", on_delete=models.CASCADE, db_column="id_Genero"
-    )
-    email = models.EmailField(unique=True, max_length=50, blank=False, null=False)
-    ubicacion = models.CharField(blank=False, name=False, max_length=50)
-    biografia = models.TextField(blank=True, null=True)
-    fecha_registro = models.DateTimeField(auto_now_add=True)
-    ultima_actividad = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return str(self.id_username)
-    
-
 class Genero(models.Model):
-    id_genero = models.AutoField(db_column="idGenero", primary_key=True)
-    genero = models.CharField(max_length=20, blank=False, null=False)
+    id_genero = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
 
     def __str__(self):
-        return str(self.genero)
+        return self.nombre
     
-class Foto(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    imagen = models.ImageField(upload_to='myapp\static\myapp\img/')
-    fecha_subida = models.DateTimeField(auto_now_add=True)
+class User(models.Model):
+    id_user = models.AutoField(primary_key=True)
+    id_genero = models.ForeignKey(Genero, on_delete=models.CASCADE, related_name='id_user_genero')
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=50 )
+    fecha_nacimiento = models.DateField()
 
     def __str__(self):
-        return f"Foto de {self.usuario.id_username}"
+        return self.email
     
-class Mensaje(models.Model):
-    usuario_emisor = models.ForeignKey(Usuario, related_name='mensajes_enviados', on_delete=models.CASCADE)
-    usuario_receptor = models.ForeignKey(Usuario, related_name='mensajes_recibidos', on_delete=models.CASCADE)
-    contenido = models.TextField(blank=False, null=False)
-    fecha_envio = models.DateTimeField(auto_now_add=True)
+class Foto (models.Model):
+    id_foto = models.AutoField(primary_key=True)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_user_foto')
+    foto = models.ImageField(upload_to='myapp/static/img')
 
     def __str__(self):
-        return f"Mensaje de {self.usuario_emisor.id_username} a {self.usuario_receptor}"
-        '''return f"Mensaje de {self.usuario_emisor} a {self.usuario_receptor}"'''
+        return self.id_user.email
 
+class UsuarioSettings(models.Model):
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_user_settings')
+    id_genero = models.ForeignKey(Genero, on_delete=models.CASCADE, related_name='id_settings_genero')
+    id_foto = models.ForeignKey(Foto, on_delete=models.CASCADE, related_name='id_settings_foto')
+    foto_perfil = models.ImageField(upload_to='myapp/static/img')
+    descripcion = models.TextField()
+    carrera = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.id_user.email
+    
 class Match(models.Model):
-    usuario_1 = models.ForeignKey(Usuario, related_name='matches_1', on_delete=models.CASCADE)
-    usuario_2 = models.ForeignKey(Usuario, related_name='matches_2', on_delete=models.CASCADE)
-    fecha_match = models.DateTimeField(auto_now_add=True)
+    id_match = models.AutoField(primary_key=True)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_match_user')
+    id_user_matched = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_user_matched')
+    
+    def __str__(self):
+        return self.id_user.nombre + ' - ' + self.id_user_matched.nombre
+    
 
-    class Meta:
-        unique_together = ('usuario_1', 'usuario_2')
+class Mensaje(models.Model):
+    id_mensaje = models.AutoField(primary_key=True)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_user_mensaje')
+    id_user_matched = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_user_matched_mensaje')
+    mensaje = models.TextField()
+    fecha_mensaje = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.id_user.nombre + ' - ' + self.id_user_matched.nombre
 
-class Preferencia(models.Model):
-    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='preferencia')
-    edad_minima = models.PositiveIntegerField(blank=True, null=True)
-    edad_maxima = models.PositiveIntegerField(blank=True, null=True)
-    id_genero = models.ForeignKey(
-        "Genero", on_delete=models.CASCADE, db_column="id_Genero"
-    )
-    distancia_maxima = models.PositiveIntegerField(blank=True, null=True)
+class chat (models.Model):
+    id_chat = models.AutoField(primary_key=True)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_user_chat')
+    id_user_matched = models.ForeignKey(User, on_delete=models.CASCADE, related_name='id_user_matched_chat')
+    id_mensaje = models.ForeignKey(Mensaje, on_delete=models.CASCADE, related_name='id_mensaje_chat')
